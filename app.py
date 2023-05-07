6da79b9b324dd41420f5a0756790cc9af651a87c
JorgeGeorge98 <jorgejavitt9@gmail.com>
2023-05-07 23:45:27 +0200

    primeros estilos, generos de juego, mostrar imagenes creadas

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
 from os import environ
-
+import urllib
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = environ["OPENAI_API_KEY"]
 
-
 @app.route("/", methods=("GET", "POST"))
-#def index():
-#    if request.method == "POST":
-#        name = request.form["name"]
-#        response = openai.Completion.create(
-#            model="text-davinci-003",
-#            max_tokens=2048,
-#            prompt=generate_prompt(name),
-#            temperature=1,
-#        )
-#        return redirect(url_for("index", result=response.choices[0].text))
-#
-#    result = request.args.get("result")
-#    return render_template("index.html", result=result)
-
 def index():
+    return render_template("index.html")
+
+@app.route('/fantasia', methods=("GET", "POST"))
+def fantasia():
     if request.method == "POST":
         name = request.form["name"]
-        response = openai.Image.create(
-            prompt="Eustaquio estaba caminando a través de una profunda selva cuando de repente se encontró cara a cara con un enorme dragón. Eustaquio sabía cuales eran los resultados de enfrentarse con un dragón, pero tuvo un atisbo de valor y se preparó para lo que vendría a continuación. ¿Qué hace Eustaquio?",
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            max_tokens=3000,
+            prompt=generate_prompt(name),
+            temperature=1,
+        )
+        imgUrl = openai.Image.create(
+            prompt=response.choices[0].text,
             n=1,
             size="256x256"
         )
-        return redirect(url_for("index", result=response['data'][0]['url']))
+        return redirect(url_for("fantasia", result=response.choices[0].text, imgUrl=imgUrl['data'][0]['url']))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+    imgUrl = request.args.get("imgUrl")
+    if imgUrl is not None:
+        decoded_url = urllib.parse.unquote(imgUrl)
+    else:
+        decoded_url = None
+    
+    return render_template("fantasia.html", result=result, imgUrl=decoded_url)
 
 def generate_prompt(name):
     return """Comienza una historia de fantasia con el nombre que se introduce
@@ -43,6 +44,4 @@ Historia: Pepe caminaba por un bosque encantado cuando encontro a un ogro llamad
 Nombre: Jose
 Historia: Jose se a despertado en una mazmorra encadenado en un traje de cuero BDSM, a Jose le gusta, que hace jose?
 Nombre: {}
-Historia:""".format(
-        name.capitalize()
-    )
+Historia:""".format(name.capitalize())
